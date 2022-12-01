80274c7a138a209ff396e65824425f7360dd551a
L1MJH <lolflgc@icloud.com>
2022-12-02 02:13:38 +0800

    Initialized pysimplegui

--- game.py
+++ game.py
@@ -1,2 +1,16 @@
-#some code
-#more code
+import PySimpleGUI as sg
+
+layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
+
+# Create the window
+window = sg.Window("Migrant Brother Game", layout)
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == "OK" or event == sg.WIN_CLOSED:
+        break
+
+window.close()
