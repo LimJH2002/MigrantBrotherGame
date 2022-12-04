ef7db7b91ff71ab1e4d2211b17fb0be1a489fe3b
L1MJH <lolflgc@icloud.com>
2022-12-04 11:30:45 +0800

    Added player name feature

--- game.py
+++ game.py
@@ -1,16 +1,33 @@
 import PySimpleGUI as sg
 
-layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
+#layouts
+sg.theme('DarkTeal12')
+layout = [[sg.Text("Welcome to our ACE Migrants Mini Game!")],
+          [sg.Text("Number of Players", size = (15, 1)), sg.InputText(key='num_of_players')], 
+          [sg.Button("CONTINUE"), sg.Button("EXIT")]]
+
+layout2 = [[sg.Text("Welcome to our ACE Migrants Mini Game!")],
+          [sg.Text("Number of Players", size = (15, 1)), sg.InputText()], 
+          [sg.Button("Test")]]
 
 # Create the window
 window = sg.Window("Migrant Brother Game", layout)
+names = []
 
 # Create an event loop
 while True:
+    
     event, values = window.read()
-    # End program if user closes window or
-    # presses the OK button
-    if event == "OK" or event == sg.WIN_CLOSED:
+
+    if event == "EXIT" or event == sg.WIN_CLOSED:
         break
+
+    if event == "CONTINUE":
+        num_players = int(values['num_of_players'])
+        for i in range(num_players):
+            name = sg.popup_get_text('Enter Your Name')
+            names.append(name)
+
+    
 
 window.close()
