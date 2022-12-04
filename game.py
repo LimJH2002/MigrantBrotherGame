e6c61025aac5dc1526b3ead58981e714d2cb9646
L1MJH <lolflgc@icloud.com>
2022-12-04 11:34:46 +0800

    Added fonts

--- game.py
+++ game.py
@@ -1,9 +1,10 @@
 import PySimpleGUI as sg
 
-#layouts
+#layouts & fonts
+font = ("Arial", 15)
 sg.theme('DarkTeal12')
-layout = [[sg.Text("Welcome to our ACE Migrants Mini Game!")],
-          [sg.Text("Number of Players", size = (15, 1)), sg.InputText(key='num_of_players')], 
+layout = [[sg.Text("Welcome to our ACE Migrants Mini Game!", font=font)],
+          [sg.Text("Number of Players", size = (15, 1), font=font), sg.InputText(key='num_of_players')], 
           [sg.Button("CONTINUE"), sg.Button("EXIT")]]
 
 layout2 = [[sg.Text("Welcome to our ACE Migrants Mini Game!")],
@@ -25,7 +26,7 @@ while True:
     if event == "CONTINUE":
         num_players = int(values['num_of_players'])
         for i in range(num_players):
-            name = sg.popup_get_text('Enter Your Name')
+            name = sg.popup_get_text('Enter Your Name', font=font)
             names.append(name)
 
     
